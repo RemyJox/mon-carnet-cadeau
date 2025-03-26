29afdeae99f3ca76c35b0cdd2d70aa93a7d421ac
remyjox <remy.jox@hotmail.fr>
2025-03-26 02:53:35 +0100

    ajout des thèmes sur event

--- migrations/Version20250326010602.php
+++ migrations/Version20250326010602.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250324134343 extends AbstractMigration
+final class Version20250326010602 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20250324134343 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE theme CHANGE anniversaire name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('DROP TABLE test');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE theme CHANGE name anniversaire VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE TABLE test (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, test VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
     }
 }
